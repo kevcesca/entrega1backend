991b4453d8a3e96463fcb7ae76013d3342e63eac
kevokl <lacartax@gmail.com>
2023-03-17 23:05:58 -0600

    reentrega

--- productManager.js
+++ productManager.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 productsArray = [{
     id: 1,
     title: 'Producto ejemplo',
@@ -6,42 +8,75 @@ productsArray = [{
     thumbnail: 'https://img.asmedia.epimg.net/resizer/VfjTidyO0IAx_6_-OeGUfUkJ5fI=/1952x1098/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/MEDSQBJ7BBNMFN7PUF7JHGY2RA.jpg',
     code: '0001',
     stock: 5,
-    
+
 
 }];
 
-class ProductManager{
-    constructor(){
-        this.products = productsArray
+class ProductManager {
+    constructor() {
+        this.products = productsArray;
     }
 
-    addProduct(newProduct){
-
+    addProduct(newProduct) {
         // Validar
-        const product = this.products.find(prod=> prod.code === newProduct.code)
-        if (product){
-            return 'Existe el producto con este codigo'
+        const product = this.products.find((prod) => prod.code === newProduct.code);
+        if (product) {
+            return 'Existe el producto con este codigo';
         }
 
-        // ID autoincrementado  
+        // ID autoincrementado
         if (this.products.length === 0) {
-            this.products.push( {id: 1, ...newProduct } )
-        } else { // Ubica el ultimo elemento del array y agrega +1 al nuevo id del producto
-            this.products.push( {id: this.products[this.products.length-1].id + 1  , ...newProduct } )
+            this.products.push({ id: 1, ...newProduct });
+        } else {
+            // Ubica el ultimo elemento del array y agrega +1 al nuevo id del producto
+            this.products.push({ id: this.products[this.products.length - 1].id + 1, ...newProduct });
         }
+    }
 
+    getProduct() {
+        return this.products;
     }
-    getProduct(){
-        return this.products
-    }
-    getProductById(id){
-        const product = this.products.find(prod => prod.id === id)
-        if(!product){
-            return "No encontre el id"
+
+    getProductById(id) {
+        const product = this.products.find((prod) => prod.id === id);
+        if (!product) {
+            return 'No encontre el id';
         }
-        return product ;
+        return product;
     }
-    
+
+    deleteProduct(id) {
+        const productIndex = this.products.findIndex((prod) => prod.id === id);
+        if (productIndex === -1) {
+            return 'No encontre el id';
+        }
+        this.products.splice(productIndex, 1);
+        return 'Producto eliminado exitosamente';
+    }
+
+    updateProduct(id, updatedProduct) {
+        const productIndex = this.products.findIndex((prod) => prod.id === id);
+        if (productIndex === -1) {
+            return 'No encontre el id';
+        }
+        if (id != updatedProduct.id) {
+            console.log("Lo siento, no tienes permisos de modificar el id");
+        }
+        const updatedProductWithId = { ...updatedProduct, id };
+        this.products.splice(productIndex, 1, updatedProductWithId);
+        return 'Producto actualizado exitosamente';
+    }
+
+    createFile(filename) {
+        const fileContent = JSON.stringify(this.products);
+        fs.writeFile(`${filename}.txt`, fileContent, (err) => {
+            if (err) {
+                console.error(err);
+                return 'Error al crear el archivo';
+            }
+            console.log(`Archivo ${filename}.txt creado exitosamente`);
+        });
+    }
 }
 
 // module.exports = {
@@ -78,3 +113,29 @@ productos.addProduct({
 console.log("Todos: ", productos.getProduct());
 console.log("Por id: ", productos.getProductById(2));
 
+productos.addProduct({
+    title: 'Producto ejemplo3',
+    description: 'Este es un producto3',
+    price: 35002,
+    thumbnail: 'https://img.asmedia.epimg.net/resizer/VfjTidyO0IAx_6_-OeGUfUkJ5fI=/1952x1098/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/MEDSQBJ7BBNMFN7PUF7JHGY2RA.jpg',
+    code: '0003',
+    stock: 6,
+})
+
+productos.deleteProduct(1);
+
+console.log("Despues de borrar el id 1 y agregando un tercer producto ", productos.getProduct());
+
+productos.updateProduct(2, {
+    title: 'Producto ejemplo2 nuevo',
+    description: 'Este es un producto2 nuevo',
+    price: 35002,
+    thumbnail: 'https://img.asmedia.epimg.net/resizer/VfjTidyO0IAx_6_-OeGUfUkJ5fI=/1952x1098/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/MEDSQBJ7BBNMFN7PUF7JHGY2RA.jpg',
+    code: '0002',
+    stock: 16,
+    id: 50
+})
+
+console.log("Despues de editar el producto 2 ", productos.getProduct());
+
+productos.createFile("arreglo");
