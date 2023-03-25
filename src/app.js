11e5984e4bb41eaba023981742df4594fbb36d77
kevokl <lacartax@gmail.com>
2023-03-25 00:52:40 -0600

    entrega 3 correciones

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
-const http = require ('http');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((peticion, respuesta) => {
-    respuesta.end('primer servidor');
-})
+// Importar el array de productos
+const { productsArray } = require('./productManager.js');
 
-const PORT = 8080;
+console.log(productsArray);
 
-server.listen(PORT,() => {
-    console.log("Servidor web iniciado en el puerto 8080");
+// Endpoint para obtener todos los productos
+app.get('/products', (req, res) => {
+    const limit = req.query.limit; // Obtener el valor del query param "limit"
+
+    if (limit) {
+        // Si se recibe un límite, devolver el número de productos solicitados
+        const products = productsArray.slice(0, limit);
+        res.json({ products });
+    } else {
+        // Si no se recibe query de límite, devolver todos los productos
+        res.json({ products: productsArray });
+    }
+});
+
+// Endpoint para obtener un producto por su ID
+app.get('/products/:pid', (req, res) => {
+    const productId = req.params.pid; // Obtener el valor del parámetro de la URL
+    const product = productsArray.find((p) => p.id == productId);
+
+    if (product) {
+        res.json({ product });
+    } else {
+        res.status(404).json({ message: 'Producto no encontrado' });
+    }
+});
+
+// Iniciar el servidor en el puerto 3000
+app.listen(8080, () => {
+    console.log('Servidor iniciado en el puerto 8080');
 });
--- src/productManager.js
+++ src/productManager.js
@@ -1,16 +1,107 @@
 const fs = require('fs');
 
-productsArray = [{
-    id: 1,
-    title: 'Producto ejemplo',
-    description: 'Este es un producto',
-    price: 3500,
-    thumbnail: 'https://img.asmedia.epimg.net/resizer/VfjTidyO0IAx_6_-OeGUfUkJ5fI=/1952x1098/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/MEDSQBJ7BBNMFN7PUF7JHGY2RA.jpg',
-    code: '0001',
-    stock: 5,
-
-
-}];
+const productsArray = [
+    {
+        id: 1,
+        title: 'Producto ejemplo',
+        description: 'Este es un producto',
+        price: 3500,
+        thumbnail:
+            'https://img.asmedia.epimg.net/resizer/VfjTidyO0IAx_6_-OeGUfUkJ5fI=/1952x1098/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/MEDSQBJ7BBNMFN7PUF7JHGY2RA.jpg',
+        code: '0001',
+        stock: 5,
+    },
+    {
+        id: 2,
+        title: 'Producto 2',
+        description: 'Este es otro producto',
+        price: 2000,
+        thumbnail:
+            'https://cdn.pixabay.com/photo/2016/03/27/20/14/notebook-1280538_960_720.jpg',
+        code: '0002',
+        stock: 10,
+    },
+    {
+        id: 3,
+        title: 'Producto 3',
+        description: 'Un producto más',
+        price: 500,
+        thumbnail:
+            'https://cdn.pixabay.com/photo/2015/06/19/21/24/the-road-815297_960_720.jpg',
+        code: '0003',
+        stock: 3,
+    },
+    {
+        id: 4,
+        title: 'Producto 4',
+        description: 'El cuarto producto',
+        price: 1500,
+        thumbnail:
+            'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg',
+        code: '0004',
+        stock: 8,
+    },
+    {
+        id: 5,
+        title: 'Producto 5',
+        description: 'Otro producto más',
+        price: 1000,
+        thumbnail:
+            'https://cdn.pixabay.com/photo/2015/05/31/13/59/man-792229_960_720.jpg',
+        code: '0005',
+        stock: 2,
+    },
+    {
+        id: 6,
+        title: 'Producto 6',
+        description: 'Producto seis',
+        price: 3000,
+        thumbnail:
+            'https://cdn.pixabay.com/photo/2015/03/26/10/29/cupcakes-691601_960_720.jpg',
+        code: '0006',
+        stock: 12,
+    },
+    {
+        id: 7,
+        title: 'Producto 7',
+        description: 'Siete es un buen número',
+        price: 800,
+        thumbnail:
+            'https://cdn.pixabay.com/photo/2015/04/20/13/17/work-731198_960_720.jpg',
+        code: '0007',
+        stock: 1,
+    },
+    {
+        id: 8,
+        title: 'Producto 8',
+        description: 'El octavo producto',
+        price: 2500,
+        thumbnail:
+            'https://cdn.pixabay.com/photo/2015/03/30/12/37/lego-698841_960_720.jpg',
+        code: '0008',
+        stock: 7,
+    },
+    {
+        id: 9,
+        title: 'Producto 9 modificado',
+        description: 'Este producto ha sido modificado',
+        price: 2500,
+        thumbnail:
+            'https://cdn.pixabay.com/photo/2017/07/31/22/33/coffee-2562222_960_720.jpg',
+        code: '0009',
+        stock: 20,
+    },
+    {
+        id: 10,
+        title: 'Producto 10',
+        description: 'Nuevo producto agregado',
+        price: 1800,
+        thumbnail:
+            'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_960_720.jpg',
+        code: '0010',
+        stock: 15,
+    },
+];
 
 class ProductManager {
     constructor() {
@@ -25,12 +116,16 @@ class ProductManager {
         }
 
         // ID autoincrementado
+        let newId;
         if (this.products.length === 0) {
-            this.products.push({ id: 1, ...newProduct });
+            newId = 1;
         } else {
-            // Ubica el ultimo elemento del array y agrega +1 al nuevo id del producto
-            this.products.push({ id: this.products[this.products.length - 1].id + 1, ...newProduct });
+            newId = this.products[this.products.length - 1].id + 1;
         }
+
+        const productWithId = { id: newId, ...newProduct };
+        this.products.push(productWithId);
+        return productWithId;
     }
 
     getProduct() {
@@ -55,12 +150,12 @@ class ProductManager {
     }
 
     updateProduct(id, updatedProduct) {
+        if (id !== updatedProduct.id) {
+            return 'Lo siento, no tienes permisos de modificar el id';
+        }
         const productIndex = this.products.findIndex((prod) => prod.id === id);
         if (productIndex === -1) {
             return 'No encontre el id';
-        }
-        if (id != updatedProduct.id) {
-            console.log("Lo siento, no tienes permisos de modificar el id");
         }
         const updatedProductWithId = { ...updatedProduct, id };
         this.products.splice(productIndex, 1, updatedProductWithId);
@@ -79,11 +174,13 @@ class ProductManager {
     }
 }
 
+
+
 // module.exports = {
 //     ProductManager
 // }
 
-const productos = new ProductManager;
+// const productos = new ProductManager;
 
 
 
@@ -101,41 +198,44 @@ const productos = new ProductManager;
 // console.log(productos.getProduct());
 
 
-productos.addProduct({
-    title: 'Producto ejemplo2',
-    description: 'Este es un producto2',
-    price: 35002,
-    thumbnail: 'https://img.asmedia.epimg.net/resizer/VfjTidyO0IAx_6_-OeGUfUkJ5fI=/1952x1098/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/MEDSQBJ7BBNMFN7PUF7JHGY2RA.jpg',
-    code: '0002',
-    stock: 6,
-})
+// productos.addProduct({
+//     title: 'Producto ejemplo2',
+//     description: 'Este es un producto2',
+//     price: 35002,
+//     thumbnail: 'https://img.asmedia.epimg.net/resizer/VfjTidyO0IAx_6_-OeGUfUkJ5fI=/1952x1098/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/MEDSQBJ7BBNMFN7PUF7JHGY2RA.jpg',
+//     code: '0002',
+//     stock: 6,
+// })
 
-console.log("Todos: ", productos.getProduct());
-console.log("Por id: ", productos.getProductById(2));
+// console.log("Todos: ", productos.getProduct());
+// console.log("Por id: ", productos.getProductById(2));
 
-productos.addProduct({
-    title: 'Producto ejemplo3',
-    description: 'Este es un producto3',
-    price: 35002,
-    thumbnail: 'https://img.asmedia.epimg.net/resizer/VfjTidyO0IAx_6_-OeGUfUkJ5fI=/1952x1098/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/MEDSQBJ7BBNMFN7PUF7JHGY2RA.jpg',
-    code: '0003',
-    stock: 6,
-})
+// productos.addProduct({
+//     title: 'Producto ejemplo3',
+//     description: 'Este es un producto3',
+//     price: 35002,
+//     thumbnail: 'https://img.asmedia.epimg.net/resizer/VfjTidyO0IAx_6_-OeGUfUkJ5fI=/1952x1098/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/MEDSQBJ7BBNMFN7PUF7JHGY2RA.jpg',
+//     code: '0003',
+//     stock: 6,
+// })
 
-productos.deleteProduct(1);
+// productos.deleteProduct(1);
 
-console.log("Despues de borrar el id 1 y agregando un tercer producto ", productos.getProduct());
+// console.log("Despues de borrar el id 1 y agregando un tercer producto ", productos.getProduct());
 
-productos.updateProduct(2, {
-    title: 'Producto ejemplo2 nuevo',
-    description: 'Este es un producto2 nuevo',
-    price: 35002,
-    thumbnail: 'https://img.asmedia.epimg.net/resizer/VfjTidyO0IAx_6_-OeGUfUkJ5fI=/1952x1098/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/MEDSQBJ7BBNMFN7PUF7JHGY2RA.jpg',
-    code: '0002',
-    stock: 16,
-    id: 50
-})
+// productos.updateProduct(2, {
+//     title: 'Producto ejemplo2 nuevo',
+//     description: 'Este es un producto2 nuevo',
+//     price: 35002,
+//     thumbnail: 'https://img.asmedia.epimg.net/resizer/VfjTidyO0IAx_6_-OeGUfUkJ5fI=/1952x1098/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/MEDSQBJ7BBNMFN7PUF7JHGY2RA.jpg',
+//     code: '0002',
+//     stock: 16,
+//     id: 50
+// })
 
-console.log("Despues de editar el producto 2 ", productos.getProduct());
+// console.log("Despues de editar el producto 2 ", productos.getProduct());
 
-productos.createFile("arreglo");
+// productos.createFile("arreglo");
+
+
+module.exports = { productsArray };
